9a08156b306f4399b69c06a21117c090a58763b9
Md. Abu Jafor Gefare <abujafor7411sec@gmail.com>
2025-12-15 23:28:57 +0600

    Implement binary search in C++

--- Search/Binary search.cpp
+++ Search/Binary search.cpp
@@ -3,9 +3,29 @@
 using namespace std;
 
 int main(){
-    int arr[10];
+    int arr[100], size, target, mid, low, high;
     cout<<"Enter the size: ";
-    cin<<
-
-
+    cin>>size;
+    cout<<"Enter "<< size <<"  elements(Sorted): ";
+    for(int i=0; i<size; i++){
+        cin>>arr[i];
+    }
+    cout<<"Enter the target: ";
+    cin>>target;
+    low = 0;
+    high = size - 1;
+    while(low <= high){
+        mid = (low + high) / 2;
+        if(arr[mid] == target){
+            cout<<"Element found at index: "<<mid<<endl;
+            return 0;
+        }
+        else if(arr[mid] < target){
+            low = mid + 1;
+        }
+        else{
+            high = mid - 1;
+        }
+    }
+    cout<<"Element not found"<<endl;
 }
